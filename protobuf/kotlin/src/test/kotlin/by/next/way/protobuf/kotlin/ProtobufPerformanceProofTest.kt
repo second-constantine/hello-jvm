e5ecdcb72d5eba057eee2dbf66832c4a320e763b
Tester Testerov <qwer@localhost>
2019-06-14 21:55:22 +0300

    Add httpclient for testing protobuf

--- protobuf/kotlin/src/test/kotlin/by/next/way/protobuf/kotlin/ProtobufPerformanceProofTest.kt
+++ protobuf/kotlin/src/test/kotlin/by/next/way/protobuf/kotlin/ProtobufPerformanceProofTest.kt
@@ -1,9 +1,15 @@
 package by.next.way.protobuf.kotlin
 
 import by.next.way.protobuf.kotlin.model.Person
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.CloseableHttpClient
+import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.logging.log4j.LogManager
 import org.junit.Assert
+import org.junit.FixMethodOrder
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.converter.HttpMessageConverter
@@ -11,19 +17,33 @@ import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.web.client.RestTemplate
 
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProtobufPerformanceProofTest {
 
+    private val client: CloseableHttpClient = HttpClientBuilder.create().build()
     @LocalServerPort
     private val port: Int = 0
+
+    @Test
+    fun aTimeForStart() {
+        log.info("Start")
+    }
 
     @Test
     fun testProtoBuf() {
         val response = RestTemplate(arrayListOf(ProtobufHttpMessageConverter()) as List<HttpMessageConverter<*>>)
                 .getForEntity(createURLWithPort("/proto_buf", port), PersonProto::class.java)
         Assert.assertNotNull(response.body)
+    }
+
+    @Test
+    fun testProtoBuf2() {
+        val request = HttpGet(createURLWithPort("/proto_buf", port))
+        val response = client.execute(request)
+        val person = PersonProto.parseFrom(response.entity.content)
+        Assert.assertNotNull(person)
     }
 
     @Test
@@ -35,6 +55,7 @@ class ProtobufPerformanceProofTest {
 
     companion object {
 
+        private val log = LogManager.getLogger()
         private fun createURLWithPort(uri: String, port: Int): String {
             return "http://localhost:$port$uri"
 
